68d6cbacc8f1343748c1c2b7ac7366ed630aed43
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2020-10-17 11:42:12 +0200

    Update map_buffer_into_process.h

--- inject_shellcode/src/map_buffer_into_process.h
+++ inject_shellcode/src/map_buffer_into_process.h
@@ -15,17 +15,19 @@ PVOID map_buffer_into_process1(HANDLE hProcess, LPBYTE buffer, SIZE_T buffer_siz
     maxSize.HighPart = 0;
     maxSize.LowPart = static_cast<DWORD>(buffer_size);
     NTSTATUS status = NULL;
+    
+    // Create a new section with specific access rights:
     if ((status = ZwCreateSection( &hSection, SECTION_ALL_ACCESS, NULL, &maxSize, protect, SEC_COMMIT, NULL)) != STATUS_SUCCESS)
     {
-        printf("[ERROR] ZwCreateSection failed, status : %x\n", status);
+        printf("[ERROR] ZwCreateSection failed, status : %x\n", status); // Failed to.
         return NULL;
     }
 
-    PVOID sectionBaseAddress = NULL;
+    PVOID sectionBaseAddress = NULL; // Variable to remote address.
     ULONG viewSize = 0;
     SECTION_INHERIT inheritDisposition = ViewShare; //VIEW_SHARE
 
-    // map the section in context of current process:
+    // Map the section in context of current process:
     if ((status = NtMapViewOfSection(hSection, GetCurrentProcess(), &sectionBaseAddress, NULL, NULL, NULL, &viewSize, inheritDisposition, NULL, protect)) != STATUS_SUCCESS)
     {
         printf("[ERROR] NtMapViewOfSection failed, status : %x\n", status);
@@ -33,10 +35,11 @@ PVOID map_buffer_into_process1(HANDLE hProcess, LPBYTE buffer, SIZE_T buffer_siz
     }
     printf("Section BaseAddress: %p\n", sectionBaseAddress);
 
+    // Copy the contents of the buffer to a new section:
     memcpy (sectionBaseAddress, buffer, buffer_size);
     printf("Buffer copied!\n");
 
-    //map the new section into context of opened process
+    // Map the new section into context of opened process
     PVOID sectionBaseAddress2 = NULL;
     if ((status = NtMapViewOfSection(hSection, hProcess, &sectionBaseAddress2, NULL, NULL, NULL, &viewSize, ViewShare, NULL, protect)) != STATUS_SUCCESS)
     {
@@ -44,21 +47,25 @@ PVOID map_buffer_into_process1(HANDLE hProcess, LPBYTE buffer, SIZE_T buffer_siz
         return NULL;
     }
 
-    //unmap from the context of current process
+    // Unmap from the context of current process:
     ZwUnmapViewOfSection(GetCurrentProcess(), sectionBaseAddress);
     ZwClose(hSection);
 
     printf("Section mapped at address: %p\n", sectionBaseAddress2);
+    // Return the address where the section was remotely mapped (or NULL, in case of failure):
     return sectionBaseAddress2;
 }
 
 LPVOID map_buffer_into_process2(HANDLE hProcess, LPBYTE buffer, SIZE_T buffer_size, DWORD protect = PAGE_EXECUTE_READWRITE)
 {
+    // Allocate memory space in remote process:
     LPVOID remoteAddress = VirtualAllocEx(hProcess, NULL, buffer_size, MEM_COMMIT | MEM_RESERVE, protect);
     if (remoteAddress == NULL)  {
+        // It didn't work, get out.
         printf("Could not allocate memory in the remote process\n");
         return NULL;
     }
+    // Write the value of the buffer to the allocated memory
     if (!WriteProcessMemory(hProcess, remoteAddress, buffer, buffer_size, NULL)) {
         VirtualFreeEx(hProcess,remoteAddress, buffer_size, MEM_FREE);
         return NULL;
